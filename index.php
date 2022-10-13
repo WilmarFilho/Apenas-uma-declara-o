91f57427ae7daf1db65571cb4814a4998d28dc4c
Wilmar Filho <wilmarfilho32@hotmail.com>
2022-10-13 12:25:21 -0300

    Visual da tela de login

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Apenas uma declaração</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div id='main' class="text-center row container"> 
+		
+		<div id='login' class="row justify-content-center col-md-10 col-11">
+			
+			<h2 class="col-md-10 col-12 text-white titulo">Oii minha plincesa</h2>
+			
+			<div class="card col-md-10 col-12 p-2 text-center mt-4">
+				<form method="POST" action="">
+					
+					<label class="label-login">Coloque uma data importante pra nós :</label>
+					<input class="form-control input-login" type="text" name="data" placeholder="Digite nossa data">
+					<button class="btn btn-outline-danger btn-block mt-2" type="submit">Entrar</button>
+
+				</form>
+			</div>
+
+		</div>
+
+
+	</div>
+</body>
+</html>
